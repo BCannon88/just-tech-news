d341953f3aaa446630d0758865da6f67e46ef18f
Baron C <cannonbaron@gmail.com>
2021-07-12 17:54:50 -0500

    added index.js to models folder

--- models/index.js
+++ models/index.js
@@ -1,7 +1,18 @@
-// Requiring User model @ User.js
-const User = require('./User');
 // Requiring Post model @ Post.js
 const Post = require("./Post");
+// Requiring User model @ User.js
+const User = require('./User');
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+// Reverse association of hte above statement
+// In this statement, we are defining the relationship of the Post model to the User. The constraint we impose here is that a post can belong to one user, but not many users. Again, we declare the link to the foreign key, which is designated at user_id in the Post model.
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });  
+
 
 
 module.exports = { User, Post };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
-// Setting up server to test user-routes.js
+// Setting up server to test user-routes.js and post-routes.js
 
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
