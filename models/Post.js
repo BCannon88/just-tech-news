9b246c9ebbfeee854918582ea90681348b1c8c77
Baron C <cannonbaron@gmail.com>
2021-07-12 18:08:08 -0500

    adding cooment.js and route file as well as vote file

--- models/Post.js
+++ models/Post.js
@@ -1,55 +1,153 @@
-// Create Post Model
-
-// connection to MySQL we stored in the connection.js file. 
 const { Model, DataTypes } = require('sequelize');
-// Model and Datatypes we'll use from the sequelize package.
 const sequelize = require('../config/connection');
-
 // create our Post model
-class Post extends Model {}
-
-// Now we will define the columns in the Post, configure the naming conventions, and pass the current connection instance to initialize the Post model.
+class Post extends Model {
+  static upvote(body, models) {
+    return models.Vote.create({
+      user_id: body.user_id,
+      post_id: body.post_id
+    }).then(() => {
+      return Post.findOne({
+        where: {
+          id: body.post_id
+        },
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [
+            sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+            'vote_count'
+          ]
+        ],
+        include: [
+          {
+            model: models.Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: models.User,
+              attributes: ['username']
+            }
+          }
+        ]
+      });
+    });
+  }
+}
 
 // create fields/columns for Post model
 Post.init(
-    {
-        //define the Post schema
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      //define the title column as a String value
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      //Then we include the post_url field, which is also defined as a String
-      post_url: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          isURL: true
-        }
-      },
-      //this column determines who posted the news article. Using the references property, we establish the relationship between this post and the user by creating a reference to the User model, specifically to the id column that is defined by the key property, which is the primary key. The user_id is conversely defined as the foreign key and will be the matching link.
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    post_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true
       }
     },
-    {
-    //In the second parameter of the init method, we configure the metadata, including the naming conventions.
-      sequelize,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'post'
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
     }
-  );
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+  }
+);
 
-  //Lastly, we must include the export expression to make the Post model accessible to other parts of the application.
-  module.exports = Post;
+module.exports = Post;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Create Post Model
+
+// // connection to MySQL we stored in the connection.js file. 
+// const { Model, DataTypes } = require('sequelize');
+// // Model and Datatypes we'll use from the sequelize package.
+// const sequelize = require('../config/connection');
+
+// // create our Post model
+// class Post extends Model {}
+
+// // Now we will define the columns in the Post, configure the naming conventions, and pass the current connection instance to initialize the Post model.
+
+// // create fields/columns for Post model
+// Post.init(
+//     {
+//         //define the Post schema
+//       id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//         primaryKey: true,
+//         autoIncrement: true
+//       },
+//       //define the title column as a String value
+//       title: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//       },
+//       //Then we include the post_url field, which is also defined as a String
+//       post_url: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         validate: {
+//           isURL: true
+//         }
+//       },
+//       //this column determines who posted the news article. Using the references property, we establish the relationship between this post and the user by creating a reference to the User model, specifically to the id column that is defined by the key property, which is the primary key. The user_id is conversely defined as the foreign key and will be the matching link.
+//       user_id: {
+//         type: DataTypes.INTEGER,
+//         references: {
+//           model: 'user',
+//           key: 'id'
+//         }
+//       }
+//     },
+//     {
+//     //In the second parameter of the init method, we configure the metadata, including the naming conventions.
+//       sequelize,
+//       freezeTableName: true,
+//       underscored: true,
+//       modelName: 'post'
+//     }
+//   );
+
+//   //Lastly, we must include the export expression to make the Post model accessible to other parts of the application.
+//   module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -1,18 +1,101 @@
-// Requiring Post model @ Post.js
-const Post = require("./Post");
-// Requiring User model @ User.js
+// import all models
+const Post = require('./Post');
 const User = require('./User');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
-    foreignKey: 'user_id'
-  });
-// Reverse association of hte above statement
-// In this statement, we are defining the relationship of the Post model to the User. The constraint we impose here is that a post can belong to one user, but not many users. Again, we declare the link to the foreign key, which is designated at user_id in the Post model.
+  foreignKey: 'user_id'
+});
+
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
-  });  
+  foreignKey: 'user_id'
+});
+
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
 
 
 
-module.exports = { User, Post };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Requiring Post model @ Post.js
+// const Post = require("./Post");
+// // Requiring User model @ User.js
+// const User = require('./User');
+
+// // create associations
+// User.hasMany(Post, {
+//     foreignKey: 'user_id'
+//   });
+// // Reverse association of hte above statement
+// // In this statement, we are defining the relationship of the Post model to the User. The constraint we impose here is that a post can belong to one user, but not many users. Again, we declare the link to the foreign key, which is designated at user_id in the Post model.
+// Post.belongsTo(User, {
+//     foreignKey: 'user_id',
+//   });  
+
+
+
+// module.exports = { User, Post };
