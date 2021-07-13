41e06366b630525b339641bd59f65bfd46acfb32
Baron C <cannonbaron@gmail.com>
2021-07-13 18:34:30 -0500

    Populate the Template with Sequelize Data

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 // This file is meant to collect the packaged API routes
 
 const router = require('express').Router();
+const homeRoutes = require('./home-routes.js');
 
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 // Importing ROUTES
-
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+
+// For Handlebars.js template
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
 
 // turn on routes
 app.use(routes);
